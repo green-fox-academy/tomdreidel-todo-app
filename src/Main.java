c3a55ad54114789fb3dcf55aefe0c53a3251e4e0
tomdreidel <redly.tamas@gmail.com>
2017-09-28 11:50:19 +0200

    usage description added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello todos!");
-
+    Todo todo = new Todo();
+    System.out.println(todo.usage());
+    
   }
-
 }
---
+++ src/Todo.java
@@ -0,0 +1,10 @@
+public class Todo {
+
+  public Todo() {
+
+  }
+
+  public String usage() {
+    return "Command Line Todo Application\n=============================\n\nCommand line arguments:\n-l\tLists all the tasks\n-a\tAdds a new task\n-r\tRemoves a task\n-c\tCompletes a task";
+  }
+}
